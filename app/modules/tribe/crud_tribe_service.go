88a60d59a7a0b9d2713be98d655568e5ff6bf05f
dewasa98 <dewasa98@gmail.com>
2019-10-16 15:50:53 +0700

    added tribeID in response createtribe

--- app/modules/tribe/crud_tribe_service.go
+++ app/modules/tribe/crud_tribe_service.go
@@ -7,7 +7,7 @@ import (
 )
 
 // CreateTribe create tribe
-func (h *Handler) CreateTribe(tribe tCreate) error {
+func (h *Handler) CreateTribe(tribe tCreate) (uint, error) {
 	// Migrate local tribe to gorm struct
 	newTribe := models.Tribe{}
 
@@ -22,18 +22,18 @@ func (h *Handler) CreateTribe(tribe tCreate) error {
 	}
 
 	if dbc := h.DB.Create(&newTribe); dbc.Error != nil {
-		return dbc.Error
+		return 0, dbc.Error
 	}
 
 	if tribe.LeadID != 0 {
 		lead := models.User{}
 		if err := h.DB.First(&lead, tribe.LeadID); err.RowsAffected == 0 {
-			return errors.New("user lead does not exist, tribe created without lead")
+			return 0, errors.New("user lead does not exist, tribe created without lead")
 		}
 		h.DB.Model(&newTribe).Association("Leads").Append(models.TribeLeadAssign{LeadID: lead.ID, TribeID: newTribe.ID})
 		h.DB.Model(&lead).Association("Tribes").Append(models.TribeAssign{UserID: lead.ID, TribeID: newTribe.ID})
 	}
-	return nil
+	return newTribe.ID, nil
 }
 
 // DeleteTribe delete tribe
@@ -53,3 +53,12 @@ func UpdateValue(updateTribe *models.Tribe, tribe *models.Tribe) {
 		tribe.Description = updateTribe.Description
 	}
 }
+
+// // GetTribeID returns tribe ID
+// func (h *Handler) GetTribeID(tribeName string) (uint, error) {
+// 	tribe := models.Tribe{}
+// 	if row := h.DB.Where("tribe_name = ?", tribeName).First(&tribe); row.RowsAffected == 0 {
+// 		return 0, errors.New("tribe does not exist")
+// 	}
+// 	return tribe.ID, nil
+// }
