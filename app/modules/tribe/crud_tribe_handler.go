33c8388dfc9a09454a167ad83d93d11d2dc4dd3d
dewasa98 <dewasa98@gmail.com>
2019-10-16 12:17:54 +0700

    gettribebyuser moved to userhandler

--- app/modules/tribe/crud_tribe_handler.go
+++ app/modules/tribe/crud_tribe_handler.go
@@ -278,56 +278,6 @@ func (h *Handler) GetTribeByID(w http.ResponseWriter, r *http.Request) {
 	helpers.RenderJSON(w, write, http.StatusOK)
 }
 
-// GetTribeByUser get users tribe
-func (h *Handler) GetTribeByUser(w http.ResponseWriter, r *http.Request) {
-	// Get User ID
-	uid, _, err := auth.ExtractTokenUID(r)
-	if err != nil {
-		helpers.SendError(w, "error uid extraction", http.StatusInternalServerError)
-		return
-	}
-
-	var tribeAss []models.TribeAssign
-	var gotTribe = false
-	if row := h.DB.Table("tribe_assigns").Find(&tribeAss, uid).RowsAffected; row != 0 {
-		gotTribe = true
-	}
-
-	resp := map[string]interface{}{"status": gotTribe}
-	resp["tribeAssign"] = tribeAss
-	write, _ := json.Marshal(resp)
-	helpers.RenderJSON(w, write, http.StatusOK)
-}
-
-// GetTribeByUserID get users tribe
-func (h *Handler) GetTribeByUserID(w http.ResponseWriter, r *http.Request) {
-	// Get User ID
-	_, role, err := auth.ExtractTokenUID(r)
-	if err != nil {
-		helpers.SendError(w, "error uid extraction", http.StatusInternalServerError)
-		return
-	}
-	if role < 1 {
-		helpers.SendError(w, "super admin access only", http.StatusForbidden)
-		return
-	}
-
-	params := mux.Vars(r)
-	var tribeAss []models.TribeAssign
-	var gotTribe = false
-
-
-	if row := h.DB.Table("tribe_assigns").Find(&tribeAss, params["user_id"]).RowsAffected; row != 0 {
-		gotTribe = true
-	}
-
-	resp := map[string]interface{}{"status": gotTribe}
-	resp["tribeAssign"] = tribeAss
-	write, _ := json.Marshal(resp)
-	helpers.RenderJSON(w, write, http.StatusOK)
-}
-
-
 // TODO GET USER BY EMAIL
 
 // AssignUser assign user in tribe by lead
--- main.go
+++ main.go
@@ -70,51 +70,41 @@ func main() {
 	router.HandleFunc("/api", index).Methods("GET") // route to test if API is alive or not
 
 	router.HandleFunc("/api/login", authHandler.Login).Methods("POST") //Login user
+	s.HandleFunc("/api/user/valid", authHandler.ValidateToken).Methods("GET") //Validate token user has
 
-	//Validate token user has
-	s.HandleFunc("/api/user/valid", authHandler.ValidateToken).Methods("GET")
 
-	//Create User
-	router.HandleFunc("/api/users", userHandler.CreateUserHandler).Methods("POST")
-
+	router.HandleFunc("/api/users", userHandler.CreateUserHandler).Methods("POST") //Create User
 	// IMPROVE: Get users by filter
 	// router.HandleFunc("/api/users", userHandler.GetUsers).Methods("GET")
 
 	s.HandleFunc("/api/users/{user_id:[0-9]+}", userHandler.DeleteUserHandler).Methods("DELETE") // Delete User
 	s.HandleFunc("/api/users/{user_id:[0-9]+}", userHandler.GetUserByID).Methods("GET") //Get user By ID
 	s.HandleFunc("/api/users/{user_id:[0-9]+}", userHandler.UpdateUserByID).Methods("PUT") //Update User
+	s.HandleFunc("/api/tribes/user/{user_id:[0-9]+}", userHandler.GetTribeByUserID) // Get user affiliated tribes
+	s.HandleFunc("/api/tribes/user", userHandler.GetTribeByUser).Methods("GET") // Get tribe by userid(GET METHOD, depend on auth token)
 	
 
-	s.HandleFunc("/api/users/{user_id:[0-9]+}/keys", keyHandler.GetKeysByUserID).Methods("GET") //Get user keys by ID // TODO implement filter
+	s.HandleFunc("/api/users/{user_id:[0-9]+}/keys", keyHandler.GetKeysByUserID).Methods("GET") //Get user keys by ID // TODO: implement filter
+	s.HandleFunc("/api/tribes/{tribe_id:[0-9]+}/keys", keyHandler.GetKeysByTribeID).Methods("GET") // Get keys from tribe ID // TODO: implement filter
 
-	// Get keys from tribe ID
-	// TODO implement filter
-	s.HandleFunc("/api/tribes/{tribe_id:[0-9]+}/keys", keyHandler.GetKeysByTribeID).Methods("GET")
-
-	//Create Tribe
-	s.HandleFunc("/api/tribes", tribeHandler.CreateTribeHandler).Methods("POST")
-	//Delete Tribe
-	s.HandleFunc("/api/tribes/{tribe_id:[0-9]+}", tribeHandler.DeleteTribeHandler).Methods("DELETE")
-
-	//Update Tribe
-	s.HandleFunc("/api/tribes/{tribe_id:[0-9]+}", tribeHandler.UpdateTribeByID).Methods("PUT")
-
+	
+	s.HandleFunc("/api/tribes", tribeHandler.CreateTribeHandler).Methods("POST") //Create Tribe
+	s.HandleFunc("/api/tribes/{tribe_id:[0-9]+}", tribeHandler.DeleteTribeHandler).Methods("DELETE") //Delete Tribe
+	s.HandleFunc("/api/tribes/{tribe_id:[0-9]+}", tribeHandler.UpdateTribeByID).Methods("PUT") //Update Tribe
 	// TODO: implement this
-	// Get Tribes
-	// s.HandleFunc("/api/tribes", tribeHandler.CreateTribeHandler).Methods("POST")
+		// Get Tribes
 
-	// TODO ADD IN DOCS
+	// TODO: ADD IN DOCS
 	s.HandleFunc("/api/tribes/{tribe_id:[0-9]+}/leads", tribeHandler.AddTribeLead).Methods("POST") // Assign Lead
 	s.HandleFunc("/api/tribes/{tribe_id:[0-9]+}/leads", tribeHandler.RemoveTribeLead).Methods("DELETE") // Remove Lead
 	s.HandleFunc("/api/tribes/{tribe_id:[0-9]+}/members", tribeHandler.AssignUser).Methods("POST") //Assign user to tribe
 	s.HandleFunc("/api/tribes/{tribe_id:[0-9]+}/members", tribeHandler.RemoveAssign).Methods("DELETE") //Remove user from tribe
 	s.HandleFunc("/api/tribes/{tribe_id:[0-9]+}", tribeHandler.GetTribeByID).Methods("GET") // Get tribe by tribe id
-	s.HandleFunc("/api/tribes/user", tribeHandler.GetTribeByUser).Methods("GET") // Get tribe by userid(GET METHOD, depend on auth token)
-	s.HandleFunc("/api/tribes/user/{user_id:[0-9]+}", tribeHandler.GetTribeByUserID) // Get user affiliated tribes
+	
 
-	// TODO Get tribe keys
+	// TODO: Get tribe keys
 	// router.HandleFunc("/api/tribe/{tribe_id:[0-9]+}/keys").Methods("GET")
-	// TODO Get tribe users
+	// TODO: Get tribe users
 	// router.HandleFunc("/api/tribe/{tribe_id:[0-9]+}/users").Methods("GET")
 
 	// TODO: ADD FILTER, FILTER BY tribe, version, key_type, platform, status
